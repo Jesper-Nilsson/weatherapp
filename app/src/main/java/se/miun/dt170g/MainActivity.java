275b4b9089ac9787a98730b5a1b87c9e9dde54e6
Ab03beed <abdaher2000@gmail.com>
2024-01-18 23:02:35 +0100

    Changed back everything to default!

--- app/src/main/java/se/miun/dt170g/MainActivity.java
+++ app/src/main/java/se/miun/dt170g/MainActivity.java
@@ -3,9 +3,6 @@ package se.miun.dt170g;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-
-import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    }
-
-    public void onClick(View v){
 
     }
 
