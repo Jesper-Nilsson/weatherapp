4e5ddc81ed55c489276df8ea621b81ca095e9d9b
jesper <jeni2001@student.miun.se>
2024-01-19 15:58:21 +0100

    added handler to WeatherApiFetcher
    - Lucas, Jesper

--- app/src/main/java/se/miun/dt170g/MainActivity.java
+++ app/src/main/java/se/miun/dt170g/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WeatherApiFetcher test  = new WeatherApiFetcher();
-        test.fetchAndParseWeatherData("https://api.met.no/weatherapi/locationforecast/2.0/classic?lat=59.93&lon=10.72&altitude=90");
+
 
 
     }
