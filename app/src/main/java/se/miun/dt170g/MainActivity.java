e81897fd19b4e830b989418dda775ff43be5c0a5
KORAY-AMAN-ASLAN <aman.arabzadeh98@gmail.com>
2024-01-19 12:07:04 +0100

    Design

--- app/src/main/java/se/miun/dt170g/MainActivity.java
+++ app/src/main/java/se/miun/dt170g/MainActivity.java
@@ -1,16 +1,31 @@
 package se.miun.dt170g;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Define TextViews for displaying weather data
+    private TextView tvTemperature, tvWindSpeed, tvCloudiness, tvPrecipitation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
+        // Initialize TextViews
+        tvTemperature = findViewById(R.id.tvTemperature);
+        tvWindSpeed = findViewById(R.id.tvWindSpeed);
+        tvCloudiness = findViewById(R.id.tvCloudiness);
+        tvPrecipitation = findViewById(R.id.tvPrecipitation);
 
+
+
+    }
 }
