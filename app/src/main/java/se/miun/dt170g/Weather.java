c0f247380637d95934a1ced7239ba56d3668e6d1
jesper <jeni2001@student.miun.se>
2024-01-19 11:48:54 +0100

    added logic to choose image based on rain and cloudiness level, (not tested yet), done by Lucas and Jesper

--- app/src/main/java/se/miun/dt170g/Weather.java
+++ app/src/main/java/se/miun/dt170g/Weather.java
@@ -22,7 +22,44 @@ public class Weather {
     }
 
     private void chooseWeatherImage(){
+        /**
+         * 0-20% no cloud
+         * 20-60 medium cloud
+         * 60-100 heavy cloudy
+         *
+         * < 0.5 no rain
+         * 0.5 - 2.5 light rain
+         * 2.5 - 7.5 medium rain
+         * > 7.5 heavy rain
+         */
+        double avgRain = (precipitation.first + precipitation.second) /2;
+        String filename;
 
+
+        if (cloudiness < 20) {
+            filename = "noCloud";
+        }
+        else if(cloudiness >= 20 && cloudiness < 60) {
+            filename = "mediumCloud";
+        }
+        else {
+            filename = "heavyCloud";
+        }
+
+        if(avgRain <= 0.5) {
+            filename += "noRain";
+
+        } else if(avgRain <= 2.5) {
+            filename += "lightRain";
+
+        } else if(avgRain <= 7.5) {
+            filename += "mediumRain";
+
+        } else {
+            filename += "heavyRain";
+        }
+
+        this.weatherImage = filename  + ".png";
     }
 
     public double getTemprature() {
